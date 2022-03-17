89d94d1abad4fc0f24b040d00edf87cbfac76984
Alexander Baron <alexander.baron1@rwth-aachen.de>
2022-03-17 19:40:07 +0100

    Add mock API endpoint

---
+++ apps/server/src/app/expected_dates.ts
@@ -0,0 +1,3 @@
+export default function getExpectedDates(courseName: string): string[] | null {
+  return ['Sample 1', 'Sample 2', courseName];
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,14 @@
 import Fastify from 'fastify';
+import invariant from 'tiny-invariant';
+import getExpectedDates from './app/expected_dates';
 
 const fastify = Fastify({ logger: true });
 
-fastify.get('/', (_, rep) => {
-  rep.send('Hello World!');
+fastify.get('/expected_date/:course_name', (req, rep) => {
+  invariant(req.params);
+  const { course_name } = req.params as Record<string, string>;
+
+  rep.send(getExpectedDates(course_name));
 });
 
 fastify.listen(3000, (err) => {
