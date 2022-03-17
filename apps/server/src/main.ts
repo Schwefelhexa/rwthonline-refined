03c8560c825e5cfb40b8ee6ad04a0657e2bbe263
Alexander Baron <alexander.baron1@rwth-aachen.de>
2022-03-17 19:31:02 +0100

    Basic web server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', (_, rep) => {
+  rep.send('Hello World!');
+});
+
+fastify.listen(3000, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
